19fa4544b365d9d29684e4e50c6cf7ca35a7e598
lincreyna2905 <lincolnreyna@icloud.com>
2023-07-14 09:14:41 -0400

    stage

--- battleship.py
+++ battleship.py
@@ -1,5 +1,4 @@
 grid =[]
-row = ""
 
 for x in range(5):
     grid.append(["0"] * 5)
@@ -15,32 +14,33 @@ print("Let's play Battleship!")
     
 createGrid(grid)
 
-row = int(input("Enter the row that you want to strike: "))
-
-def checkRow(row):
-        if row > 4 | row < 0:
-            print("Enter a number between 0-4")
-            
-        
-    
-    
 
 while True:
+    guessRow = int(input("Which row do you want to strike: "))
+    guessColumn = int(input("Which column do you want to strike: "))
     
-    row = int(input("Enter the row that you want to strike: ")
-    checkRow(row)
-   
-    column = int(input("Enter the column that you want to strike: "))
-
-   
+    if grid[guessRow][guessColumn] == "x":
+            print("You already guessed right there, try again")
+            
+    elif guessRow in (0, 1, 2, 3, 4) and guessColumn in (0, 1, 2, 3, 4):
+        grid[guessRow][guessColumn] = "x"
+        print("you struck at ", guessRow, ",", guessColumn)
         
-    if grid[row][column] == "x":
-        print("You already guessed right there")
-        
-        
+    else:
+        if guessRow != (0, 1, 2, 3, 4):
+            print("Please enter a number between 0-4")
+            guessRow = int(input("Which row do you want to strike: "))
+            
+        if guessColumn != (0, 1, 2, 3, 4):
+            print("PLease enter a number between 0-4")
+            guessColumn = int(input("Which column do you want to strike: "))
+    
+    createGrid(grid)        
+    
+    
+                
+            
 
 
-
-    grid[row][column] = "x"
-    createGrid(grid)
+    
 
