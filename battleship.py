bc7b64487700e20b72cc684eb2e64d188d80176f
lincreyna2905 <lincolnreyna@icloud.com>
2023-07-18 11:29:21 -0400

    Stage 1 modified

--- battleship.py
+++ battleship.py
@@ -1,4 +1,7 @@
-grid =[]
+from random import randint
+
+grid = []
+
 
 for x in range(5):
     grid.append(["0"] * 5)
@@ -8,18 +11,29 @@ def createGrid(grid):
         for column in rows:
             print(column, end=" ")
         print()
+        
 
 
 print("Let's play Battleship!")
+
+def ran_row(grid):
+    return randint(0, len(grid - 1))
     
+def ran_col(grid):
+    return randint(0, len(grid[0] - 1))
+        
+ship_row = ran_row
+ship_col = ran_col
+
+
 createGrid(grid)
-while True:
+for turn in range(5):
     
-    
+
     while True:
         try:
-            guessRow = int(input("Which row do you want to strike: "))
-            guessColumn = int(input("Which column do you want to strike: "))
+            guessRow = eval(input("Which row do you want to strike: "))
+            guessColumn = eval(input("Which column do you want to strike: "))
             
             
             if guessRow < 0 or guessRow > 4 or guessColumn < 0 or guessColumn > 4:
@@ -30,28 +44,27 @@ while True:
         except ValueError:
             print('Please enter a number between 0-4')
             
-            
-    while True:
         
-        if grid[guessRow][guessColumn] == "x":
-                print("You already guessed right there, try again")
-                
-        elif guessRow in (0, 1, 2, 3, 4) and guessColumn in (0, 1, 2, 3, 4):
-            grid[guessRow][guessColumn] = "x"
-            print("you struck at ", guessRow, ",", guessColumn)
+    if grid[guessRow][guessColumn] == "x":
+            print("You already guessed right there, try again")
             
-        else:
-            if guessRow != (0, 1, 2, 3, 4):
-                print("Please enter a number between 0-4")
-                guessRow = int(input("Which row do you want to strike: "))
-                
-            if guessColumn != (0, 1, 2, 3, 4):
-                print("PLease enter a number between 0-4")
-                guessColumn = int(input("Which column do you want to strike: "))
-                
-        break
+    elif guessRow in (0, 1, 2, 3, 4) and guessColumn in (0, 1, 2, 3, 4):
+        grid[guessRow][guessColumn] = "x"
+        print("you struck at ", guessRow, ",", guessColumn)
         
-    createGrid(grid)        
+    elif (guessRow == ship_row) and (guessColumn == ship_col):
+        print("Yay! You sunk my battleship!")
+        
+    else:
+        if guessRow != (0, 1, 2, 3, 4):
+            print("Please enter a number between 0-4")
+            guessRow = int(input("Which row do you want to strike: "))
+            
+        if guessColumn != (0, 1, 2, 3, 4):
+            print("PLease enter a number between 0-4")
+            guessColumn = int(input("Which column do you want to strike: "))
+
+    createGrid(grid)              
     
     
     
